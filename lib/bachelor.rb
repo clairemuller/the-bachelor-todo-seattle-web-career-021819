f8ef1ed33859e69b63ee86b144d380c28d623998
clairemuller <clairermuller@gmail.com>
2019-02-13 16:11:25 -0800

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,75 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season_str, contestants|
+    if season_str == season
+      contestants.each do |contestant_hash|
+        contestant_hash.each do |k, v|
+          if k == "status" && v == "Winner"
+            winner = contestant_hash["name"].split
+            return winner[0]
+          end
+        end
+      end
+    end
+  end
 end
+
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_str, contestants|
+    contestants.each do |contestant_hash|
+      contestant_hash.each do |k, v|
+        if k == "occupation" && v == occupation
+          return contestant_hash["name"]
+        end
+      end
+    end
+  end
 end
+
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  count = 0
+  data.each do |season_str, contestants|
+    contestants.each do |contestant_hash|
+      contestant_hash.each do |k, v|
+        if k == "hometown" && v == hometown
+          count += 1
+        end
+      end
+    end
+  end
+  count
 end
+
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season_str, contestants|
+    contestants.each do |contestant_hash|
+      contestant_hash.each do |k, v|
+        if k == "hometown" && v == hometown
+          return contestant_hash["occupation"]
+        end
+      end
+    end
+  end
 end
 
+
 def get_average_age_for_season(data, season)
-  # code here
+  ages = []
+  data.each do |season_str, contestants|
+    if season_str == season
+      contestants.each do |contestant_hash|
+        contestant_hash.each do |k, v|
+          if k == "age"
+            ages << v.to_i
+          end
+        end
+      end
+    end
+  end
+  ages = ages.inject{ |sum, el| sum + el }.to_f / ages.length
+  ages.round
 end
